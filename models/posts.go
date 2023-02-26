4a27cb13c35f40463cc3c3754ec065f07df31bad
xDouglas90 <odouglas.dev@gmail.com>
2023-02-26 08:48:53 -0300

    refactor: change ID to integer :recycle:

---
+++ models/posts.go
@@ -0,0 +1,17 @@
+package models
+
+type Post struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+var Posts []Post
+
+func init() {
+	Posts = []Post{
+		{ID: 1, Title: "Post 1", Content: "Content 1"},
+		{ID: 2, Title: "Post 2", Content: "Content 2"},
+		{ID: 3, Title: "Post 3", Content: "Content 3"},
+	}
+}
