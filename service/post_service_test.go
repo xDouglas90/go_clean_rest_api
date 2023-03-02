4225bdac276d0934cf2704c119b400e3b21a27a6
xDouglas90 <odouglas.dev@gmail.com>
2023-03-02 20:08:10 -0300

    test(post_service): add FindAll test :white_check_mark:

--- service/post_service_test.go
+++ service/post_service_test.go
@@ -4,12 +4,59 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/xdouglas90/gomux-rest-api/entity"
 )
 
+type MockPostRepository struct {
+	mock.Mock
+}
+
+func (mock *MockPostRepository) Save(post *entity.Post) (*entity.Post, error) {
+	args := mock.Called(post)
+	result := args.Get(0)
+
+	return result.(*entity.Post), args.Error(1)
+}
+
+func (mock *MockPostRepository) FindAll() ([]entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+
+	return result.([]entity.Post), args.Error(1)
+}
+
+func TestFindAll(t *testing.T) {
+	mockRepo := new(MockPostRepository)
+	var identifier int64 = 1
+	post := entity.Post{ID: identifier, Title: "Test", Content: "Hello World"}
+
+	// Setup expectations
+	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
+
+	testService := NewPostService(mockRepo)
+
+	result, _ := testService.FindAll()
+
+	// Mock Assertion: Behavioral
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.Equal(t, identifier, result[0].ID)
+	assert.Equal(t, "Test", result[0].Title)
+	assert.Equal(t, "Hello World", result[0].Content)
+}
+
+// func TestSave(t *testing.T) {
+// 	mockRepo := new(MockPostRepository)
+// 	post := entity.Post{ID: 1, Title: "Test", Content: "Hello World"}
+
+// 	// Setup expectations
+// 	mockRepo.On("FindAll").Return(entity.Post{post}, nil)
+// }
+
 func TestValidateEmptyPost(t *testing.T) {
 	testService := NewPostService(nil)
-
 	err := testService.Validate(nil)
 
 	assert.NotNil(t, err)
@@ -18,9 +65,7 @@ func TestValidateEmptyPost(t *testing.T) {
 
 func TestValidateEmptyPostTitle(t *testing.T) {
 	testService := NewPostService(nil)
-
 	post := entity.Post{ID: 1, Title: "", Content: "Hello World"}
-
 	err := testService.Validate(&post)
 
 	assert.NotNil(t, err)
